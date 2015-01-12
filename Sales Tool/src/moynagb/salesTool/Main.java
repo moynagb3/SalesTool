aebbf7db7ef7b8b03be52ad430feaf87fe220ede
moynagb3 <brian.moynagh3@mail.dcu.ie>
2015-01-12 12:25:37 +0000

    Initial Check in sales tool

---
+++ Sales Tool/src/moynagb/salesTool/Main.java
@@ -0,0 +1,17 @@
+package moynagb.salesTool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+
+	private static void displayGreeting() {
+		System.out.println("Hello Happy Sales People");
+		System.out.println("This app displays sales data");
+	}
+	
+}
---
+++ Sales Tool/src/moynagb/salesTool/SalesData.java
@@ -0,0 +1,11 @@
+package moynagb.salesTool;
+
+public class SalesData {
+
+	int  [] data = {9, 4, 42};
+	
+	public void display() {
+		System.out.println("TODO - implement data display");
+	}
+	
+}
